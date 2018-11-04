6f459d97923cbb1bc4445148c36335657f15b3ac
Luke <whyinlijun@163.com>
2018-11-04 23:22:01 +0800

    add sqlite_console

--- get_item_sale_count.py
+++ get_item_sale_count.py
@@ -13,36 +13,71 @@
         淘宝搜索页面    付款人数为 前一日结束付款人数之和,已减掉退款人数
         店铺销量排序页面 销售件数, 前一日结束销售件数之和,已减掉退款件数
 
-
-
 商品详情页信息
 https://detailskip.taobao.com/service/getData/1/p1/item/detail/sib.htm?itemId=567069635897&sellerId=99778076&modules=dynStock,qrcode,viewer,price,duty,xmpPromotion,delivery,upp,activity,fqg,zjys,couponActivity,soldQuantity,originalPrice,tradeContract&callback=onSibRequestSuccess
 
 '''
 
-import requests ,re ,json
+import requests, re, json, time
+import sqlite_console as sql_c
 
-url = "https://tui.taobao.com/recommend"
+IS_PRINT = True
 
-headers = {
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-}
+def get_data(itemID):
+    url = "https://tui.taobao.com/recommend"
+    headers = {
+                "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    }
+    payload = {
+                'type': 2,
+                'count': 25,
+                'itemId': itemID,
+                #'scm': '1007.10008.56612.103200300000000',
+                'appid': 1502,
+                #'_ksTS': '1524663427460_44',
+                'callback': 'jsonp45',
+    }
+    while True:
+        w = requests.get(url , params = payload , headers = headers)
+        re_c = re.compile("\((.*)\)")
+        re_d = re.search(re_c, w.text).group(1)
+        re_dict = json.loads(re_d)
+        if "orderCost" in re_dict['detail']:
+            break
+        time.sleep(2)
 
-payload = {
-            #'type': 1,
-            #'count': 25,
-            'itemId': 569609605234,#567069635897,
-            #'scm': '1007.10008.56612.103200300000000',
-            'appid': 1502,
-            #'_ksTS': '1524663427460_44',
-            'callback': 'jsonp45',
-}
+    if IS_PRINT:
+        for item in re_dict['detail']:
+            print('{}:{}'.format(item,re_dict['detail'][item]))
 
-w = requests.get(url , params = payload , headers = headers)
-re_c = re.compile("\((.*)\)")
-re_d = re.search(re_c,w.text).group(1)
-re_dict = json.loads(re_d)
-print("")
-for item in re_dict['detail']:
-    print('{}:{}'.format(item,re_dict['detail'][item]))
+    sale_detail = {
+        'id': re_dict['detail']['itemId'],
+        'title': re_dict['detail']['itemName'],
+        'sell_id': re_dict['detail']['sellerId'],
+        'category': re_dict['detail']['categoryId'],
+        'url': re_dict['detail']['url'],
+        'pic': re_dict['detail']['pic'],
+        'price': re_dict['detail']['price'],
+        'promotion_price': re_dict['detail']['promotionPrice'],
+        'sale_count': re_dict['detail']['sellCount'],
+        'month_count': re_dict['detail']['monthSellCount'],
+        'favorite_count': re_dict['detail']['orderCost'],
+    }
+    return sale_detail
 
+def main():
+    sale_detail = get_data('558470001549')
+    data=[]
+    data.append((sale_detail['id'],
+          int(sale_detail['month_count']),
+          int(sale_detail['sale_count']),
+          int(sale_detail['favorite_count']),
+          time.strftime("%Y-%m-%d %H:%M:%S")
+          ))
+    zhang_wei_fu = ','.join(['?' for x in range(5)])
+    sql = "INSERT INTO count_items VALUES({})".format(zhang_wei_fu)
+    conn = sql_c.get_conn(sql_c.DB_FILE_PATH)
+    sql_c.insert_order(conn, sql, data)
+
+if __name__ == "__main__":
+    main()
---
+++ sqlite_console.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python3
+#coding : utf-8
+
+'''
+本库为sqlite3操作数据库
+
+'''
+
+import os, sqlite3
+
+DB_FILE_PATH = '/home/luck/test.db'
+SHOW_SQL = True
+
+def get_conn(path=""):
+    if path != '':
+        print('{}在硬盘上'.format(path))
+        conn = sqlite3.connect(path)
+    else:
+        print('在内存上')
+        conn = sqlite3.connect(":memory:")
+    return conn
+
+def create_sql(conn, sql):
+    if sql is not None and sql != '':
+        with conn:
+            c = conn.cursor()
+            c.execute(sql)
+            if SHOW_SQL:
+                print('执行sql: [{}]'.format(sql))
+            conn.commit()
+    else:
+        print("[{}] is empty or equal is None!".format(sql))
+
+
+def insert_order(conn, sql, values):
+    if sql is not None and sql != '':
+        with conn:
+            c = conn.cursor()
+            c.executemany(sql,values)
+            conn.commit()
+
+def init(conn):
+    create_sql_items = '''CREATE TABLE  IF NOT EXISTS `items` ( 
+        `id` varchar(20) PRIMARY KEY,
+        `title` varchar(40),
+        `sell_id` varchar(20),
+        `category` varchar(20),
+        `url` varchar(100),
+        `pic` varchar(100),
+        `price` float,
+        `promo_price` float,
+        `month_count` int,
+        `sale_count` int,
+        `favorite` int
+        )'''
+    create_sql(conn,create_sql_items)
+    create_sql_count_items = ''' CREATE TABLE IF NOT EXISTS `count_items`(
+        id, month_count, sale_count_,favorite_count,order_date
+        )'''
+    create_sql(conn, create_sql_count_items)
+
+
+def main():
+    zhan_wei_fu = ','.join(['?' for item in range(11)])
+    insert_sql = "INSERT INTO `items` VALUES({})".format(zhan_wei_fu)
+    values = [('558470001549',
+              '新款中老年羽绒马甲男士大码秋冬季羽绒背心加厚保暖坎肩马夹内胆',
+              '839762818',
+              '50011153',
+              'http://item.taobao.com/item.htm?scm=1007.11502.21311.100200300000003&id=558470001549&pvid=9728a94a-d8de-4899-aba6-3407d406b502',
+              'http://img.alicdn.com/bao/uploaded/i1/839762818/TB22grDXamgSKJjSsphXXcy1VXa_!!839762818.jpg',
+              198.0,
+              198.0,
+              3222,
+              35917,
+              21916),]
+    conn = get_conn(DB_FILE_PATH)
+    insert_order(conn, insert_sql, values)
+    #init(conn)
+
+if __name__ == "__main__":
+    main()
+
